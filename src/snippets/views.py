13540d86adb6b630980a660ca3b2772ec9cb9be3
truongvan <truongvan@live.com>
2019-05-19 23:19:02 +0700

    using generics of REST framework

--- src/snippets/views.py
+++ src/snippets/views.py
@@ -1,40 +1,23 @@
 from django.http import Http404
 from rest_framework import status
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import (CreateModelMixin, DestroyModelMixin,
-                                   ListModelMixin, RetrieveModelMixin,
-                                   UpdateModelMixin)
+from rest_framework.generics import (ListCreateAPIView,
+                                     RetrieveUpdateDestroyAPIView)
 
 from .models import Snippet
 from .serializers import SnippetSerializer
 
 
-class SnippetList(ListModelMixin, CreateModelMixin, GenericAPIView):
+class SnippetList(ListCreateAPIView):
     """
     List all snippets, or create a new snippet.
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list( request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class SnippetDetail(RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, GenericAPIView):
+class SnippetDetail(RetrieveUpdateDestroyAPIView):
     """
     Retrive, update, delete a code snippet
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)

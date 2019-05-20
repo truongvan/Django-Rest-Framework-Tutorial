fcfb97cf176ac335813e3c22a8eb563379c2aa2a
truongvan <truongvan@live.com>
2019-05-20 20:21:58 +0700

    1. Fixing typo errors in models and serializer 2. Delete unuse code

--- src/snippets/models.py
+++ src/snippets/models.py
@@ -3,7 +3,6 @@ from django.utils.translation import ugettext_lazy as _
 from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
 
-
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGES_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
@@ -13,7 +12,7 @@ class Snippet(models.Model):
     created = models.DateTimeField(_("Create"), auto_now=False, auto_now_add=True)
     title = models.CharField(_("Title"), max_length=100, blank=True, default='')
     code = models.TextField(_("Code"))
-    lineos = models.BooleanField(_("Lineos"), default=False)
+    linenos = models.BooleanField(_("Linenos"), default=False)
     language = models.CharField(_("Language"), max_length=100, choices=LANGUAGES_CHOICES, default='python')
     style = models.CharField(_("Style"), max_length=100, choices=STYLE_CHOICES, default='friendly')
 
--- src/snippets/serializers.py
+++ src/snippets/serializers.py
@@ -2,28 +2,7 @@ from rest_framework import serializers
 from .models import Snippet, LANGUAGES_CHOICES, STYLE_CHOICES
 
 
-# class SnippetSerializer(serializers.Serializer):
-#     id = serializers.IntegerField(read_only=True)
-#     title = serializers.CharField(required=True)
-#     code = serializers.CharField(style={'base_template': 'textarea.html'})
-#     lineos = serializers.BooleanField(required=False)
-#     language = serializers.ChoiceField(choices=LANGUAGES_CHOICES, default='python')
-#     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
-
-#     def create(self, validated_data):
-#         return Snippets.object.create(**validated_data)
-
-    
-#     def update(self, instance, validated_data):
-#         instance.title = validated_data.get('title', instance.title)
-#         instance.code = validated_data.get('code', instance.title)
-#         instance.language= validated_data.get('language', instance.title)
-#         instance.style = validated_data.get('style', instance.title)
-#         instance.lineos = validated_data.get('lineos', instance.title)
-#         instance.save()
-#         return instance
-
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
-        fields = ('id', 'created', 'title', 'code', 'lineos', 'language', 'style')
+        fields = ('id', 'created', 'title', 'code', 'linenos', 'language', 'style')
